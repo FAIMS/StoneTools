5928c06570ab4a8e561980d88549df12e28a419c
vincentt143 <tran.vincent17@gmail.com>
2014-02-18 15:52:05 +1100

    Fix saving collections

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-uuid="bbd7f0d9-726e-42df-9e8d-229dc5c42cfb"
+uuid="45cd8000-196e-4d6d-a644-fa7dc0edd13b"
 `adb push ui_schema.xml /sdcard/faims/modules/$uuid/ui_schema.xml`
 `adb push data_schema.xml /sdcard/faims/modules/$uuid/data_schema.xml`
 `adb push ui_logic.bsh /sdcard/faims/modules/$uuid/ui_logic.bsh`
