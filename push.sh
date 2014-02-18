f5dd3256fc1b8214ea1d87e9b079cda0b402cb7a
vincentt143 <tran.vincent17@gmail.com>
2014-02-18 17:12:16 +1100

    Add CollectionArtefact relationships

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-uuid="45cd8000-196e-4d6d-a644-fa7dc0edd13b"
+uuid="4c22732c-5b12-4ca9-bd03-c1bc9180fb03"
 `adb push ui_schema.xml /sdcard/faims/modules/$uuid/ui_schema.xml`
 `adb push data_schema.xml /sdcard/faims/modules/$uuid/data_schema.xml`
 `adb push ui_logic.bsh /sdcard/faims/modules/$uuid/ui_logic.bsh`
