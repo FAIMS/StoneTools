ee62e48bd4e8eaf6a09ba1db8cdafb5f3bfd583e
vincentt143 <tran.vincent17@gmail.com>
2014-02-18 14:41:20 +1100

    Remove Person ArchEnt, change tab groups

---
+++ push.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+`adb push ui_schema.xml /sdcard/faims/modules/237cb62e-2b57-46f2-92a0-917a012a4136/ui_schema.xml`
+`adb push data_schema.xml /sdcard/faims/modules/237cb62e-2b57-46f2-92a0-917a012a4136/data_schema.xml`
+`adb push ui_logic.bsh /sdcard/faims/modules/237cb62e-2b57-46f2-92a0-917a012a4136/ui_logic.bsh`
