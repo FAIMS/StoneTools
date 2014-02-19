fc8ba659df58ec58197a16cd27ab8f29f6cd530c
vincentt143 <tran.vincent17@gmail.com>
2014-02-19 14:05:52 +1100

    Started Report Tab

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-uuid="b7d774cf-73d9-4aa9-812e-a0d94c5d8b89"
+uuid="446c229d-8a2e-48c0-a11d-1e582e1ac036"
 `adb push ui_schema.xml /sdcard/faims/modules/$uuid/ui_schema.xml`
 `adb push data_schema.xml /sdcard/faims/modules/$uuid/data_schema.xml`
 `adb push ui_logic.bsh /sdcard/faims/modules/$uuid/ui_logic.bsh`
