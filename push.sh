e9d0c6e56128bdd6782a4a0eb3abe2c044d8ea10
vincentt143 <tran.vincent17@gmail.com>
2014-02-19 15:47:23 +1100

    Can now access artefacts/reports from first tabgroup

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-uuid="4c0caae9-e2e1-42de-8c54-7868aa2f4d52"
+uuid="059c24cf-dd73-4a88-8cd6-fe0eaf439878"
 `adb push ui_schema.xml /sdcard/faims/modules/$uuid/ui_schema.xml`
 `adb push data_schema.xml /sdcard/faims/modules/$uuid/data_schema.xml`
 `adb push ui_logic.bsh /sdcard/faims/modules/$uuid/ui_logic.bsh`
