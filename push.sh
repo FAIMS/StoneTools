00ad80269a7cea91bad4de1f428b8a8431b65805
vincentt143 <tran.vincent17@gmail.com>
2014-02-19 14:42:46 +1100

    I believe we are done

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-uuid="446c229d-8a2e-48c0-a11d-1e582e1ac036"
+uuid="4c0caae9-e2e1-42de-8c54-7868aa2f4d52"
 `adb push ui_schema.xml /sdcard/faims/modules/$uuid/ui_schema.xml`
 `adb push data_schema.xml /sdcard/faims/modules/$uuid/data_schema.xml`
 `adb push ui_logic.bsh /sdcard/faims/modules/$uuid/ui_logic.bsh`
